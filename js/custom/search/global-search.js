ac410e22ed3902f1570c8511bf165afbc1c41134
Akash Kalia <akash130821@gmail.com>
2021-12-15 00:45:28 -0500

    Global Search Skeleton Added

--- js/custom/search/global-search.js
+++ js/custom/search/global-search.js
@@ -4,17 +4,30 @@ import { $ } from './../helpers.js';
 //todo
 //global search implementation
 export function global_search() {
-    const data_object = dataObject;
+    const selectedCat = String(new URLSearchParams(window.location.search).get('query'));
+    if (selectedCat) {
+        const data_object = dataObject;
+        const searchResult = {};
+        Object.keys(dataObject).forEach((cat) => {
+            if (data_object[cat]['categories']) {
+                data_object[cat]['categories'].forEach((type) => {
+                    searchResult[data_object[cat]['title'] + '~' + type['title']] = type['products'].filter((product) => {
+                        return String(product.title).toLowerCase().includes(searchValue) || String(product.short_desc).toLowerCase().includes(searchValue)
+                    });
+                })
+            }
+        });
+        console.table(searchResult);
+    } else {
+        alert('Please enter a valid search query!');
+    }
+}
+
+export function global_search_handler() {
     const searchValue = String($('search-inpt').value).toLowerCase();
-    const searchResult = {};
-    Object.keys(dataObject).forEach((cat) => {
-        if (data_object[cat]['categories']) {
-            data_object[cat]['categories'].forEach((type) => {
-                searchResult[data_object[cat]['title'] + '~' + type['title']] = type['products'].filter((product) => {
-                    return String(product.title).toLowerCase().includes(searchValue) || String(product.short_desc).toLowerCase().includes(searchValue)
-                });
-            })
-        }
-    });
-    console.table(searchResult);
+    if (searchValue && String(searchValue).trim().length > 0) {
+        window.location.href = `https://wt-ecommerce.netlify.app/global-search.html?query=${searchValue}`
+    } else {
+        alert('Please enter a product to srarch!');
+    }
 }
