b0a18a71d7448ff9fd7216f203c03efaadbc8ba8
Akash Kalia <akash130821@gmail.com>
2021-12-15 01:11:51 -0500

    Global Search Data Object Prepared

--- js/custom/search/global-search.js
+++ js/custom/search/global-search.js
@@ -4,10 +4,13 @@ import { $ } from './../helpers.js';
 //todo
 //global search implementation
 export function global_search() {
-    const selectedCat = String(new URLSearchParams(window.location.search).get('query'));
-    if (selectedCat) {
+    const searchValue = String(new URLSearchParams(window.location.search).get('query'));
+    if (searchValue) {
         const data_object = dataObject;
         const searchResult = {};
+        let minPrice = 1000000;
+        let maxPrice = 0;
+        const finalSeggrigation = new Map();
         Object.keys(dataObject).forEach((cat) => {
             if (data_object[cat]['categories']) {
                 data_object[cat]['categories'].forEach((type) => {
@@ -17,7 +20,18 @@ export function global_search() {
                 })
             }
         });
-        console.table(searchResult);
+        Object.keys(searchResult).forEach((key) => {
+            if (searchResult[key].length > 0) {
+                if (finalSeggrigation.has(key.split("~")[0])) {
+                    finalSeggrigation.get(key.split("~")[0]).set(key.split("~")[1], searchResult[key])
+                } else {
+                    cat = new Map();
+                    cat.set(key.split("~")[1], searchResult[key]);
+                    finalSeggrigation.set(key.split("~")[0], cat);
+                }
+            }
+        });
+        console.log(finalSeggrigation)
     } else {
         alert('Please enter a valid search query!');
     }
